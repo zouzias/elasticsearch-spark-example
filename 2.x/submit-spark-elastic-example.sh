e2ef63affc5326c971d9ecfc841e770d39b309db
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-03-12 18:21:02 +0100

    2.x: init commit

---
+++ 2.x/submit-spark-elastic-example.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Directory where spark-submit is defined
+# Install spark from https://spark.apache.org/downloads.html
+#
+# Works in mac
+# TODO: fix it for linux
+USER=$(whoami)
+SPARK_HOME=/Users/${USER}/spark-1.5.1-bin-hadoop2.6
+
+# JAR containing a simple hello world
+JARFILE=`pwd`/target/scala-2.11/elasticsearch-spark-example-assembly-0.1.0.jar
+
+# Run it locally
+${SPARK_HOME}/bin/spark-submit --class org.zouzias.elasticsearch.spark.ElasticSparkHelloWorld --master local $JARFILE
