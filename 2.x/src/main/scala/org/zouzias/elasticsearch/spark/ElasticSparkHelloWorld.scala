158a3b200dcb77a0b10ba3004c0f56c5bed3e61f
Anastasios Zouzias <zouzias@gmail.com>
2017-05-13 10:08:57 +0200

    2.x: add example

--- 2.x/src/main/scala/org/zouzias/elasticsearch/spark/ElasticSparkHelloWorld.scala
+++ 2.x/src/main/scala/org/zouzias/elasticsearch/spark/ElasticSparkHelloWorld.scala
@@ -1,24 +1,33 @@
 package org.zouzias.elasticsearch.spark
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.elasticsearch.spark.sql._
+
 
 object ElasticSparkHelloWorld {
   def main(args: Array[String]) {
 
     // initialise spark context
-    val conf = new SparkConf().setAppName("SparkElasticHelloWorld")
-    val sc = new SparkContext(conf)
+    val conf = new SparkConf().setAppName(ElasticSparkHelloWorld.getClass.getName)
+    val spark = SparkSession.builder().config(conf).getOrCreate()
 
-    // do stuff
-    println("************")
-    println("************")
-    println("Hello, world!")
-    println("************")
-    println("************")
+    // Elastic connection parameters
+    val elasticConf: Map[String, String] = Map("es.nodes" -> "localhost",
+      "es.clustername" -> "elasticsearch")
+
+    val indexName = "test_index"
+    val mappingName = "test_index_type"
+
+
+    // Load DataFrame
+    val df = spark.sqlContext.read.parquet("PATH_TO_DATAFRAME")
+
+    // Write elasticsearch
+    df.saveToEs(s"${indexName}/${mappingName}", elasticConf)
+
 
     // terminate spark context
-    sc.stop()
-
+    spark.stop()
   }
-
 }
