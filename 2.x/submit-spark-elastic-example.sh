9050d10274b002ee593241279f111632dc04a1e6
Anastasios Zouzias <zouzias@gmail.com>
2017-05-21 13:52:28 +0200

    version update

--- 2.x/submit-spark-elastic-example.sh
+++ 2.x/submit-spark-elastic-example.sh
@@ -6,7 +6,7 @@
 # Works in mac
 # TODO: fix it for linux
 USER=$(whoami)
-SPARK_HOME=/Users/${USER}/spark-1.5.1-bin-hadoop2.6
+SPARK_HOME=/Users/${USER}/spark-2.1.1-bin-hadoop2.7
 
 # JAR containing a simple hello world
 JARFILE=`pwd`/target/scala-2.11/elasticsearch-spark-example-assembly-0.1.0.jar
